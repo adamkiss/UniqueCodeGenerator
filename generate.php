23477d87fdfbdac88d5fe25df5421d3d36967c9d
Adam Kiss <iam@adamkiss.com>
2017-04-25 14:20:20 +0200

    Finish v1

--- generate.php
+++ generate.php
@@ -5,7 +5,7 @@
 	if (is_null($count)) { $count = 20; }
 	if (is_null($length)) { $length = 6; }
 	if (is_null($name)) { $name = 'output-'.time(); }
-	if (is_null($format)) { $format = 'xls'; }
+	if (is_null($format)) { $format = 'xlsx'; }
 
 	$UNIQUE = [];
 	$md5_base = time();
@@ -19,4 +19,22 @@
 		while (array_key_exists($new_code, $UNIQUE));
 
 		$UNIQUE []= $new_code;
+	}
+
+	require_once __DIR__ . '/vendor/autoload.php';
+	$output_file = __DIR__ . "/output/$name.$format";
+	switch ($format) {
+		case 'xlsx': case 'xls':
+			$output = [];
+			foreach($UNIQUE as $u){
+				$output []= ['kod'=>$u, 'pouzitie'=>' '];
+			}
+			$opts = [
+				'title' => "{$count} unikatnych kodov pre {$name}",
+				'subject' => "{$count} unikatnych kodov pre {$name}",
+				'creator' => 'Unique Code Generator v1'
+			];
+			\DPRMC\Excel::simple($output, [], 'Kody', $output_file, $opts);
+		break; default:
+			file_put_contents($output_file, implode("\n", $UNIQUE));
 	}
